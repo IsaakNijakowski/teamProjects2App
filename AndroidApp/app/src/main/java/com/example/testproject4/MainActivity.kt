fabe795140a927e8375f1f010855bd9a169599ef
Isaak Nijakowski <nijakowski.2@wright.edu>
2025-04-15 09:08:42 -0400

    Adjusted width, made host only on local

--- AndroidApp/app/src/main/java/com/example/testproject4/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/example/testproject4/MainActivity.kt
@@ -6,7 +6,9 @@ import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.webkit.WebViewClientCompat
@@ -29,6 +31,7 @@ class MainActivity : ComponentActivity() {
 fun MyWebView(url: String) {
     AndroidView(factory = { context ->
         WebView(context).apply {
+            Modifier.fillMaxWidth()
             webViewClient = WebViewClientCompat()
             settings.javaScriptEnabled = true
             settings.allowContentAccess = true
